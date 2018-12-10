52daac62421b7ae44e73abeb95c592bcede3c427
Samuel <js.rosemond@gmail.com>
2018-12-10 00:38:32 -0500

    Setting up react practice apps for future

---
+++ React-portfolio/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './Styles/styles.scss';
+
+ReactDOM.render(<p>Portfolio</p>, document.getElementById('app'));
