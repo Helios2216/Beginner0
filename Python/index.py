22445845aa2158371ff6314cd69ba8df718b2fc8
Samuel <js.rosemond@gmail.com>
2018-12-22 14:09:04 -0500

    Python basics p1

---
+++ Python/index.py
@@ -0,0 +1,15 @@
+# is to comment out in .py
+# print('hello world')
+# Strings in py are either single or double quotes.
+# .Py variables can't have spaces or symbols other than underscore, can't begin with a number but can have numbers after the first letter. ie. cool_variable_5
+# .Py error denoted with ^character. Syntax Error: punctuation, command where it is not expected or missing parenthesis
+# NameError: .Py sees a word it doesn't recognize || code contains something that looks like a variable but was never defined.
+# Numbers : same as Java so far int = whole num; float = float ie. an_int = 2 a_float = 2.5 print(an_int + 4)
+# Calculations: diff: .Py division .Py converts int to float before performing division. .Py will throw special error: zeroDivisionError when dividing by zero
+# Pemdas/ Exponents .Py num ** numE = exponents
+# mod = same, concate same except: str(num) to add into string if not used an integer
+# Example: myAge + str(age)
+# Triple quote = multi-line string
+# Example: multStr = ''' blah blah '''
+# .Py basics part one, I will learn .Py later
+# const byePy = later => javascript;
