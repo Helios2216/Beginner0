32eb00d95a0627aa34d40f85683d3e3de7e040ec
Samuel <js.rosemond@gmail.com>
2019-04-05 02:37:40 -0400

    Solved MultiArray Biggest on my own!

--- Algorithms/AlgorithmsLVLUP.js
+++ Algorithms/AlgorithmsLVLUP.js
@@ -39,4 +39,26 @@ function removeNumFromString(word) {
 
 }
 
-removeNumFromString('abc1234def');
+// removeNumFromString('abc1234def'); complete
+
+//Solved on my own: figure out what I was missing!!!!
+findBigNum = (arr, biggest =0) => {
+	console.log('initial Biggest:', biggest, arr);
+	
+
+	for(let el of arr) {
+		if(biggest < el ) biggest = el;
+		if(Array.isArray(el)) biggest=  findBigNum(el, biggest);
+		
+	
+		console.log({biggest}, {el});
+
+	}
+
+	
+	console.log('outside:',biggest);
+	return biggest;
+
+}
+
+console.log(findBigNum([17,[43,83],23,[62,90],82]));
