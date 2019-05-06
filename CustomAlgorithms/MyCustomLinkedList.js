87bd5b60d3ec505a2e031904dc3df87be8f7a8aa
Samuel <js.rosemond@gmail.com>
2019-05-06 09:54:34 -0400

    Link List basic + custom func 1, adding more later

---
+++ CustomAlgorithms/MyCustomLinkedList.js
@@ -0,0 +1,80 @@
+// I am adding of custom functionality to the linked list
+// As a visual learner I like to see : (clg) the internal of elements
+
+function Node(data) {
+	this.data = data;
+	this.next = null;
+}
+
+function LinkList() {
+	let length = 0;
+	let head = null;
+
+	this.add = function(item) {
+		let list = new Node(item);
+
+		if (head === null) {
+			head = list;
+		} else {
+			currentNode = head;
+
+			while (currentNode.next) {
+				currentNode = currentNode.next;
+			}
+
+			currentNode.next = list;
+		}
+		++length;
+	};
+	this.showHead = function() {
+		console.log(head);
+	};
+	this.showLength = function() {
+		console.log(length);
+	};
+	this.showItem = item => {
+		let start = head;
+
+		while (start.data !== item) {
+			if (!start.next) {
+				console.log('item not found');
+				return;
+			}
+			start = start.next;
+		}
+		console.log(start);
+	};
+	this.remove = function(item) {
+		let currentNode = head;
+		let previousNode;
+
+		if (item === currentNode.data) {
+			head = currentNode.next;
+		} else {
+			while (currentNode.data !== item) {
+				previousNode = currentNode;
+				currentNode = currentNode.next;
+			}
+
+			previousNode.next = currentNode.next;
+		}
+		--length;
+	};
+}
+
+let test = new LinkList();
+
+test.add('TEST');
+test.add('TEST2');
+test.add('Test3');
+//test.showHead();
+test.add('Test4');
+test.showLength();
+test.remove('TEST2');
+//test.showHead();
+test.showLength();
+test.add('test5');
+test.add('lala');
+test.add('huh');
+test.showItem('TEST2');
+test.showItem('lala');
