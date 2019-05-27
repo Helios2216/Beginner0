d1247c267d1351478ae6fc1e1073d0b0535657df
Samuel <js.rosemond@gmail.com>
2019-05-27 15:53:24 -0400

    Course 2: Reading files + quit

---
+++ Python_Course_2/Reading_Files.py
@@ -0,0 +1,18 @@
+# to read file call open() : returns "file handle"
+# open (filename, mode) , mode: 'r'(read), 'w'(write)
+# i.e file_handle = open('blah.txt', 'r')
+import os
+
+file = 'Beginner0/Python_Course_2/Lorem_Test.txt'
+dir_path = os.path.realpath(file)
+file_handle = open(dir_path, 'r')
+
+# \n maintained: fix: rstrip()
+print(dir_path)
+
+try:
+    for lines in file_handle:
+        print(lines.rstrip())
+except:
+    print('File not found')
+    quit()  # terminate entire program, no traceback
