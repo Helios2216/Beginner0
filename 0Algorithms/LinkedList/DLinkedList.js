9cdce1db96b50928e1901b019177a6df712df11c
Samuel <js.rosemond@gmail.com>
2019-12-03 11:55:12 -0500

    DLinkedlist get, unshift, traverse methods

--- 0Algorithms/LinkedList/DLinkedList.js
+++ 0Algorithms/LinkedList/DLinkedList.js
@@ -68,6 +68,52 @@ class DoublyLinkedList {
 
 		return oldHead;
 	}
+	unshift(value) {
+		let node = new Node(value);
+
+		if (!this.head) {
+			this.head = node;
+			this.tail = this.head;
+		} else {
+			node.next = this.head;
+			this.head.prev = node;
+			this.head = node;
+		}
+		this.length++;
+
+		return this;
+	}
+	get(index) {
+		if (index < 0 || index >= this.length) return null;
+
+		const mid = Math.floor(this.length / 2);
+
+		let node;
+
+		// > || >= one extra loop for index == middle
+		if (index > mid) {
+			node = this.traverse(index, this.tail);
+		} else {
+			node = this.traverse(index, this.head);
+		}
+
+		return node;
+	}
+	traverse(num, path) {
+		let result = path;
+
+		if (path === this.head) {
+			for (let i = 0; i < num; i++) {
+				result = result.next;
+			}
+		} else {
+			for (let i = this.length - 1; i > num; i--) {
+				result = result.prev;
+			}
+		}
+
+		return result;
+	}
 }
 
 const dList = new DoublyLinkedList();
@@ -76,4 +122,11 @@ dList.push(1);
 dList.push(2);
 dList.push(3);
 dList.push(4);
-console.log(dList);
+
+//console.log(dList);
+
+// Working get diff from Course
+console.log(dList.get(0));
+console.log(dList.get(1));
+console.log(dList.get(2));
+console.log(dList.get(3));
