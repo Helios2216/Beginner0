ce0a655f43cf82338c1ee62eca4f8f9e7cdf128c
Samuel <js.rosemond@gmail.com>
2019-12-19 12:49:40 -0500

    Tree +my traverse will update from course if error

---
+++ 0Algorithms/Tree/tree.js
@@ -0,0 +1,77 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class Tree {
+	constructor() {
+		this.root = null;
+		this.size = 0;
+	}
+	insert(value) {
+		let node = new Node(value);
+
+		if (!this.root) {
+			this.root = node;
+			++this.size;
+			return this;
+		} else {
+			let current = this.root;
+
+			while (true) {
+				if (value === current.value) {
+					return 'No duplicates allowed';
+				}
+				if (value < current.value) {
+					if (current.left === null) {
+						current.left = node;
+						++this.size;
+						return this;
+					}
+					current = current.left;
+				} else {
+					if (current.right === null) {
+						current.right = node;
+						++this.size;
+						return this;
+					}
+					current = current.right;
+				}
+			}
+		}
+	}
+	traverse(value) {
+		let current = this.root;
+
+		while (true) {
+			if (current === null) {
+				return null;
+			} else if (value === current.value) {
+				return true;
+			}
+
+			if (value < current.value) {
+				current = current.left;
+			} else {
+				current = current.right;
+			}
+		}
+	}
+}
+
+const tree = new Tree();
+tree.insert(5);
+tree.insert(3);
+tree.insert(6);
+tree.insert(1);
+tree.insert(2);
+tree.insert(0);
+tree.insert(4);
+
+console.log(tree);
+console.log(tree.insert(1));
+console.log(tree.traverse(1));
+console.log(tree.traverse(100));
