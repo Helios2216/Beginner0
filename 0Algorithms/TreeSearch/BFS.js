b23177aa04a823689dae5c479d5f26aec379e1a5
Samuel <js.rosemond@gmail.com>
2019-12-28 16:08:52 -0500

    bfs iteravite before course solution

--- 0Algorithms/TreeSearch/BFS.js
+++ 0Algorithms/TreeSearch/BFS.js
@@ -1,64 +1,56 @@
-const Tree = require('../Tree/treeExport')
-const Queue = require('../Stack_Queue/queueExport')
+const Tree = require('../Tree/treeExport');
+const Queue = require('../Stack_Queue/queueExport');
 
 class BFS extends Tree {
+	constructor(queue, queue1) {
+		super();
 
-    constructor(queue,queue1) {
-        super()
+		this.visited = queue;
+		this.queue = queue1;
+	}
 
-        this.visited = queue
-        this.queue = queue1
+	mapToArray(queue) {
+		const array = [];
+		let temp;
+		while (queue.length > 0) {
+			temp = queue.dequeue();
+			array.push(temp);
+		}
 
-        this.traverse = this.traverse.bind(this)
-    }
+		temp = null;
+		return array;
+	}
+	iTraverse() {
+		while (true) {
+			let current;
 
-    traverse() {
-   
-        let current;
+			if (this.visited.length < 1) {
+				this.queue.enqueue(this.root);
+			}
 
-        if(this.visited.length < 1) {     
-            this.queue.enqueue(this.root)
-        }
+			if (this.queue.length > 0) {
+				current = this.queue.dequeue();
+			}
 
-       if(this.queue.length > 0) {
-        current = this.queue.dequeue()
-       }
+			if (current.left) {
+				this.queue.enqueue(current.left);
+			}
+			if (current.right) {
+				this.queue.enqueue(current.right);
+			}
 
-        if(current.left) {
-            this.queue.enqueue(current.left)
-        }  
-        if(current.right) {
-            this.queue.enqueue(current.right)
-        }
-       
-        if(current) {
-            this.visited.enqueue(current.value)
-        }
-      
-        if(this.queue.length === 0) {
-            console.log(this.visited, '\n \n')
-            console.log(this.mapToArray(this.visited), '\n \n')
-           
-            return this.visited;
-           
-        
-        } else {
-            this.traverse()
-        }
-    }
-    mapToArray(queue) {
-        const array = []
-        let temp;
-        while(queue.length > 0) {
-            temp = queue.dequeue()
-            array.push(temp)
-        }
+			if (current) {
+				this.visited.enqueue(current.value);
+			}
 
-        temp = null;
-        return array;
+			if (this.queue.length === 0) {
+				//console.log(this.visited, '\n \n');
+				//console.log(this.mapToArray(this.visited), '\n \n');
 
-    }
-
+				return this.visited;
+			}
+		}
+	}
 }
 
 /* 
@@ -67,23 +59,20 @@ class BFS extends Tree {
       2   4   6  9   
 */
 
- let queue = new Queue
- let visited = new Queue
- const bfs = new BFS(visited,queue)
+let queue = new Queue();
+let visited = new Queue();
+const bfs = new BFS(visited, queue);
 
-bfs.insert(5)
-bfs.insert(3)
-bfs.insert(7)
-bfs.insert(2)
-bfs.insert(4)
-bfs.insert(6)
-bfs.insert(9)
-
-
+bfs.insert(5);
+bfs.insert(3);
+bfs.insert(7);
+bfs.insert(2);
+bfs.insert(4);
+bfs.insert(6);
+bfs.insert(9);
 
 //console.log(bfs.traverse())
 
-let answer = bfs.traverse()
+let answer = bfs.iTraverse();
 
-console.log(answer)
-
+console.log(answer);
